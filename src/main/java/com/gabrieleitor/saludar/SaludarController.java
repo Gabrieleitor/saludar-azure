f406e9bcb97a912ce831314f616d1f6fbd20783b
Gabriel <>
2026-03-27 19:18:11 -0500

    crear metrica

--- src/main/java/com/gabrieleitor/saludar/SaludarController.java
+++ src/main/java/com/gabrieleitor/saludar/SaludarController.java
@@ -1,5 +1,8 @@
 package com.gabrieleitor.saludar;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class SaludarController {
 
-	@GetMapping("/saludar/{nombre}")
-	public String saludar(@PathVariable String nombre) {
-		return "¿Cómo estás, " + nombre + "?";
-	}
+    private final Counter saludosCounter;
+    private final Timer tiempoRespuesta;
+
+    public SaludarController(MeterRegistry registry) {
+        this.saludosCounter = Counter.builder("saludar.requests.total")
+                .description("Total de requests al endpoint saludar")
+                .register(registry);
+
+        this.tiempoRespuesta = Timer.builder("saludar.response.time")
+                .description("Tiempo de respuesta del endpoint")
+                .register(registry);
+    }
+
+
+    @GetMapping("/saludar/{nombre}")
+    public String saludar(@PathVariable String nombre) {
+        saludosCounter.increment();
+        return tiempoRespuesta.record(() -> ("¿Cómo estás, " + nombre + "?"));
+    }
 }
